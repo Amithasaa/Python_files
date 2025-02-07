b3e3b56078793dc9f44a1830d9b43eb597664b4f
Amithasaa <amithaamitha290@gmail.com>
2025-02-07 14:17:23 +0530

    Final

---
+++ MultiThreading/processOrder.py
@@ -0,0 +1,11 @@
+import time
+def process_request(user):
+    print(f"Processing request for the {user}...")
+    time.sleep(5)
+    print(f"Request completed for {user}")
+    
+process_request("User 1")
+process_request("User 2")
+process_request("User 3")
+
+t1 = threading.Thread(target = process_request, args= ("User 1"))
